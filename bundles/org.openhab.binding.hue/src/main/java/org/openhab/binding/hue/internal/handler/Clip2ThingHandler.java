e628eb959d1483bf2c9509a079d8d313d44e8b8c
Andrew Fiddian-Green <software@whitebear.ch>
2023-04-02 15:25:03 +0100

    [hue] wip
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.binding.hue/src/main/java/org/openhab/binding/hue/internal/handler/Clip2ThingHandler.java
+++ bundles/org.openhab.binding.hue/src/main/java/org/openhab/binding/hue/internal/handler/Clip2ThingHandler.java
@@ -190,8 +190,8 @@ public class Clip2ThingHandler extends BaseThingHandler {
      * @param key the i18n text key.
      * @return the localized text.
      */
-    public String getLocalizedText(String key) {
-        String result = translationProvider.getText(bundle, key, key, localeProvider.getLocale());
+    public String getLocalizedText(String key, @Nullable Object @Nullable... arguments) {
+        String result = translationProvider.getText(bundle, key, key, localeProvider.getLocale(), arguments);
         return Objects.nonNull(result) ? result : key;
     }
 
@@ -697,7 +697,7 @@ public class Clip2ThingHandler extends BaseThingHandler {
             ResourceReference group = scene.getGroup();
             if (Objects.nonNull(group) && resourceId.equals(group.getId())) {
                 String label = scene.getName();
-                String description = getLocalizedText(HueBindingConstants.SCENE_ACTIVATE_KEY);
+                String description = getLocalizedText(HueBindingConstants.SCENE_ACTIVATE_KEY, label);
                 ChannelUID channelUID = new ChannelUID(thingUID, scene.getId());
                 Channel channel = ChannelBuilder.create(channelUID, CoreItemFactory.SWITCH)
                         .withType(HueBindingConstants.SCENE_CHANNEL_TYPE_UID).withLabel(label)
