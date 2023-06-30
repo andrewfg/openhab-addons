e49f1de0b25357bf79adbe3d43674b493f9c0172
Andrew Fiddian-Green <software@whitebear.ch>
2023-06-30 17:40:20 +0100

    [hue] pre merge sync
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.binding.hue/src/main/java/org/openhab/binding/hue/internal/connection/Clip2Bridge.java
+++ bundles/org.openhab.binding.hue/src/main/java/org/openhab/binding/hue/internal/connection/Clip2Bridge.java
@@ -515,13 +515,11 @@ public class Clip2Bridge implements Closeable {
      *
      * @param cancelTask the task to be cancelled (may be null)
      * @param mayInterrupt allows cancel() to interrupt the thread.
-     * @return always returns null.
      */
-    private @Nullable Future<?> cancelTask(@Nullable Future<?> cancelTask, boolean mayInterrupt) {
+    private void cancelTask(@Nullable Future<?> cancelTask, boolean mayInterrupt) {
         if (Objects.nonNull(cancelTask)) {
             cancelTask.cancel(mayInterrupt);
         }
-        return null;
     }
 
     /**
@@ -576,9 +574,11 @@ public class Clip2Bridge implements Closeable {
             }
             if (!internalRestartScheduled) {
                 // don't close the task if a restart is current
-                internalRestartTask = cancelTask(internalRestartTask, false);
+                cancelTask(internalRestartTask, false);
+                internalRestartTask = null;
             }
-            checkAliveTask = cancelTask(checkAliveTask, true);
+            cancelTask(checkAliveTask, true);
+            checkAliveTask = null;
             closeSession();
             try {
                 http2Client.stop();
@@ -1083,7 +1083,8 @@ public class Clip2Bridge implements Closeable {
     public void setExternalRestartScheduled() {
         externalRestartScheduled = true;
         internalRestartScheduled = false;
-        internalRestartTask = cancelTask(internalRestartTask, false);
+        cancelTask(internalRestartTask, false);
+        internalRestartTask = null;
         close2();
     }
 
